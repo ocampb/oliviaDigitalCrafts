a669fd0b7606147a132a741cc7ce051d656c04f9
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-06-09 10:04:40 -0400

    Update smallproblem.py

--- week1/day3/smallproblem.py
+++ week1/day3/smallproblem.py
@@ -4,52 +4,94 @@
 # 1. Sum the Numbers
 # Create a list of numbers, print their sum.
 
-pretzel = [1, 5, 6, 3, 8, 2, -1]
+# pretzel = [1, 5, 6, 3, 8, 2, -1]
+# sum = 0
 
-print (sum(pretzel))
+# for unit in pretzel: 
+#     sum = sum+unit
+  
+# print(sum)
 
 
 # 2. Largest Number
 # Create a list of numbers, print the largest of the numbers.
 
-for item in pretzel:
-    (sorted(pretzel))
-    print() pretzel;[-1]
+# biggest = 0
 
-#3. Smallest Number
-# Create a list of numbers, print the smallest of the numbers.
+# for unit in pretzel:
+#    if unit > biggest:
+#     biggest = unit
 
-for item in pretzel:
-    (sorted(pretzel))
-    print [0]
-
-#4. Even Numbers
-# Create a list of numbers, print each number in the list that is even.
-
-for item in pretzel:
-    if (item % 2) == 0:
-        print(item)
+# print (biggest)
 
 
-#5. Positive Numbers
-#Create a list of numbers, print each number in the list that is greater than zero.
 
-for item in pretzel:
-    if (item > 0):
-        print(item)
+# # #3. Smallest Number
+# # # Create a list of numbers, print the smallest of the numbers.
 
-#6. Positive Numbers II
-#Create a list of numbers, create a new list which contains every number in the given list which is positive.
+# smallest = 0
 
-for item in pretzel:
-    if (item < 0):
-        print([])
+# for unit in pretzel:
+#    if unit < smallest:
+#     smallest = unit
+
+# print (smallest)
+
+# # #4. Even Numbers
+# # # Create a list of numbers, print each number in the list that is even.
+
+# for item in pretzel:
+#    if (item % 2) == 0:
+#       print(item)
+
+
+# #5. Positive Numbers
+# #Create a list of numbers, print each number in the list that is greater than zero.
+
+# for item in pretzel:
+#     if (item > 0):
+#         print(item)
+
+
+
+# #6. Positive Numbers II
+# #Create a list of numbers, create a new list which contains every number in the given list which is positive.
+
+# list2 = []
+
+# for item in pretzel:
+#     if (item > 0):
+#         list2.append(item)
+
+# print (list2)
 
 #7. Multiply a list
 #Create a list of numbers, and a single factor (also a number), create a new list consisting of each of the numbers in the first list multiplied by the factor. Print this list.
+## example
+
+# newlist = [] 
+# dog = [1, 2, 3]
+# factor = 2
+
+# for unit in dog: 
+#      print(unit*factor)
 
 
 
-#8. Reverse a String
-#Given a string, print the string reversed.
+# 8. Reverse a String
+# Given a string, print the string reversed.
+
+me = "Olivia"
+
+# mereversed = 0
+
+# print(me[-1])
+
+# for letter in me: 
+    
+#     print(me)
+
+### example
+# input = "olivia"
+# prints "aivilo"
 
